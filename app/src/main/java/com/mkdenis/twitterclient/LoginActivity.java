4f39654c27951387f1ac9990ab0b296dd041609f
denis <kirichenko.db@gmail.com>
2017-08-11 20:03:27 +0300

    sync with twitter

--- app/src/main/java/com/mkdenis/twitterclient/LoginActivity.java
+++ app/src/main/java/com/mkdenis/twitterclient/LoginActivity.java
@@ -2,6 +2,12 @@ package com.mkdenis.twitterclient;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.twitter.sdk.android.core.DefaultLogger;
+import com.twitter.sdk.android.core.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterConfig;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +15,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        TwitterConfig config = new TwitterConfig.Builder(this)
+                .logger(new DefaultLogger(Log.DEBUG))
+                .twitterAuthConfig(new TwitterAuthConfig("CONSUMER_KEY", "CONSUMER_SECRET"))
+                .debug(true)
+                .build();
+        Twitter.initialize(config);
     }
 }
